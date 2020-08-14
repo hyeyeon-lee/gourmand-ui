4f5f9c8e277934e052ab6a45136118b90ecf1012
hyeyeon-lee <dear.yeonee@gmail.com>
2020-08-14 22:25:43 +0900

    feat: Restaurant Home 페이지 html, css 원복, css 스크립트 추가
    css: a 태그 영역이 li 영역을 감싸도록 사이즈 조정
    script: nav_bar li 말고 a마다 각각 toggle class(mouseover, mouseleave)

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
-const topMenu = document.querySelector(".top_menu>*");
-//console.log(topMenu);
-topMenu.addEventListener("mouseover", toggleMenu);
+const topMenu = document.querySelector("ul.top_menu");
+
+const topMenuList = topMenu.querySelectorAll(".top_menu > li > a");
+topMenuList.forEach((item) => {
+  item.addEventListener("mouseover", toggleMenu);
+  item.addEventListener("mouseleave", toggleMenu);
+});
 
 function toggleMenu(e) {
-  const subMenu = document.querySelector(".sub_menu");
+  const subMenu = e.target.parentNode.querySelector(".sub_menu");
   subMenu.classList.toggle("hide");
 }
